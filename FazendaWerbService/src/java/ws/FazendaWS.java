337d56959deae263f0aa7abb5eec48b9866cd849
Lucas Assis <lucasaassis@outlook.com>
2018-06-22 10:04:19 -0300

    Implementando busca de usuario através do e-mail com passagem de parametros para o webservice

--- FazendaWerbService/src/java/ws/FazendaWS.java
+++ FazendaWerbService/src/java/ws/FazendaWS.java
@@ -6,6 +6,7 @@
 package ws;
 
 import com.google.gson.Gson;
+import com.sun.org.glassfish.gmbal.ParameterNames;
 import dao.UsuarioDAO;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +17,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import modelo.Usuario;
 
 /**
@@ -47,29 +49,29 @@ public class FazendaWS {
     }
 
     @GET()
-    @Path("usuario")
+    @Path("usuario/{login}")
     @Produces("application/json")
-    public String getUsuario() {
+    public String getUsuario(@PathParam("login") String login) {
         Usuario usuario = new Usuario();
-        usuario.setEmail("l.assis@eddydata.com.br");
-        usuario.setLogin("l.assis");
-        usuario.setSenha("123");
-        usuario.setPerfil("Administrador");
+        usuario.setLogin(login);
+        
+        UsuarioDAO dao = new UsuarioDAO();
+        usuario = dao.buscar(usuario);
         
         Gson g = new Gson(); //biblioteca da Google para conversão de objeto para json
         return g.toJson(usuario);
     }
-    
+
     @GET()
-    @Path("usuario/list")
+    @Path("usuario/all")
     @Produces("application/json")
     public String getAllUsuarios() {
         List<Usuario> lista;
-        
+
         UsuarioDAO dao = new UsuarioDAO();
         lista = dao.listar();
-        
-        Gson g = new Gson(); 
+
+        Gson g = new Gson();
         return g.toJson(lista);
     }
 
